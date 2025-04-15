8db97f97c42ee9ae55458af912220875d8ee9555
blanco <dehara14@gmail.com>
2025-04-15 13:55:41 -0600

    landing page

--- pages/index.vue
+++ pages/index.vue
@@ -16,7 +16,7 @@ onMounted(() => {
 </script>
 
 <template>
-    <div ref="container" class="prose max-w-full relative p-10 overflow-clip preserve-3d perspective-[1000px]">
+    <div ref="container" class="relative p-10 overflow-clip">
         <div ref="card-1" v-motion :initial="{
             opacity: 0, rotateZ: 0, transition: {
                 type: 'spring',
@@ -33,13 +33,13 @@ onMounted(() => {
                 mass: 0.5,
                 delay: 200
             }
-        }"
-            class="sticky top-5  h-[200vh] bg-primary text-primary-content p-4 z-10 snap-start transform-gpu preserve-3d">
-            <h1 class="text-3xl font-bold">System engineers</h1>
-            <p>
-                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
-                industry's standard dummy text ever since the 1500s...
-            </p>
+        }" class="sticky top-5 h-screen bg-primary text-primary-content p-10 z-10 snap-start">
+            <div class="flex flex-col gap-4">
+                <h1 v-motion-pop-visible class="text-6xl lg:text-8xl font-extrabold" :delay="200">Systems Engineers</h1>
+                <p v-motion-pop-visible :delay="400" class="text-2xl">
+                    Mantenimiento total, rendimiento óptimo para tu equipo de cómputo.
+                </p>
+            </div>
         </div>
 
         <!-- Slide 2 -->
@@ -52,13 +52,56 @@ onMounted(() => {
                 mass: 0.5,
                 delay: 200
             },
-        }" class="sticky top-30 h-[110vh] bg-secondary text-white p-4 z-20 snap-start shadow-lg ">
-            <h1 class="text-3xl font-bold">Slide 2</h1>
-            <p>More content here...</p>
+        }" class="sticky top-10 bg-secondary p-4 z-20 snap-start shadow-lg ">
+            <div class="flex flex-col gap-4">
+                <h1 v-motion-pop-visible :delay="200" class="text-6xl font-extrabold text-secondary-content">Te
+                    ofrecemos:</h1>
+                <div class="grid grid-cols-2 lg:grid-cols-3 gap-4">
+                    <div v-motion-pop-visible :delay="200" class="card bg-base-100 w-full shadow-sm">
+                        <div class="card-body">
+                            <h2 class="card-title">Mantenimiento</h2>
+                            <p>Sí crees que tu equipo esta lento o funciona mal, realizamos un mantenimiento preventivo
+                                o correctivo para tu equipo.</p>
+                        </div>
+                        <figure>
+                            <img src="/img/repair.webp" alt="Mantenimiento" />
+                        </figure>
+                    </div>
+                    <div v-motion-pop-visible :delay="300" class="card bg-base-100 w-full shadow-sm">
+                        <div class="card-body">
+                            <h2 class="card-title">Recuperación de archivos</h2>
+                            <p>Si borraste un archivo por error, ¡nosotros lo recuperamos!</p>
+                        </div>
+                        <figure class="text-secondary">
+
+                            <img src="/img/files.webp" alt="Archivos" />
+                        </figure>
+                    </div>
+                    <div v-motion-pop-visible :delay="400" class="card bg-base-100 w-full shadow-sm">
+                        <div class="card-body">
+                            <h2 class="card-title">Actualización de hardware</h2>
+                            <p>Sí necesitas reemplazar alguna pieza de tu equipo.</p>
+                        </div>
+                        <figure>
+                            <img src="/img/hardware.webp" alt="Hardware" />
+
+                        </figure>
+                    </div>
+                    <div v-motion-pop-visible :delay="500" class="card bg-base-100 w-full shadow-sm">
+                        <div class="card-body">
+                            <h2 class="card-title">Actualización e instalación de software</h2>
+                            <p>Sí necesitas la paquetería de Office o similares.</p>
+                        </div>
+                        <figure class="bg-primary">
+                            <img src="/img/software.webp" alt="Software" class="" />
+                        </figure>
+                    </div>
+                </div>
+            </div>
         </div>
 
         <!-- Slide 3 -->
-        <div v-motion :initial="{
+        <!--         <div v-motion :initial="{
             opacity: 0, rotateZ: 0, transition: {
                 type: 'spring',
                 stiffness: 250,
@@ -81,7 +124,11 @@ onMounted(() => {
         <div class="h-[500px]">
 
 
-        </div>
+        </div> -->
+    </div>
+    <div class="sticky top-0 h-[2000px] bg-neutral text-white p-4 z-30 snap-start">
+        <h1 class="text-3xl font-bold sticky top-0">Slide 3</h1>
+        <p>Last section stacked on top</p>
     </div>
 
 </template>
